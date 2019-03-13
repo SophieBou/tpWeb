d3170968c08ac37995165736f26555f6962fc36e
Sophie <Sophie@10.41.32.45>
2019-03-13 17:51:19 +0100

    debut Q2

--- interaction.js
+++ interaction.js
@@ -8,9 +8,27 @@ this.PositionInitY =0
 this.positionFinalY =0
 this.positionFinalX =0
 	// Developper les 3 fonctions gérant les événements
+function getPression(){
+
+}
+
+function getRelachement(){
+
+}
+
+
+function getDeplacement(){
+
+}
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+	
+	canvas.addEventListener('mousedown', this.getPression, false);
+canvas.addEventListener('mousemove', this.getDeplacement, false);
+canvas.addEventListener('mouseup', this.getRelachement, false);
 };
+
+
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
